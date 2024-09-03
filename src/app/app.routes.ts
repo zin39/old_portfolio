5fe559cd70f180516e64ce5122673963403037f8
zin39 <13lack_zin@proton.me>
2024-09-03 21:46:53 +0545

    Day 2 of creating the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'Home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'About-me',
+    component: AboutMeComponent
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  },
+  {
+    path: 'Home',
+    component: HomepageComponent
+  },
+  {
+    path: 'Skills',
+    component: SkillsComponent
+  },
+  {
+    path: 'Resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'Portfolio',
+    component: PortfolioComponent
+  }
+];
